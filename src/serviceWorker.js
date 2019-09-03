c68a0634d051f937cb648565a2adeadf1fd3de50
Mayur Panchal <mayurpachal@sysadmins-MacBook-Pro.local>
2019-09-03 18:44:04 +0530

    deferred prompt code updated

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -50,13 +50,13 @@ export function register(config) {
 			deferredPrompt = e;
     });
 
-    let btnAdd = document.querySelector('#btn-home');
-    btnAdd.addEventListener('click', (e) => {
-      // hide our user interface that shows our A2HS button
-      btnAdd.style.display = 'none';
-      // Show the prompt
+    // let btnAdd = document.querySelector('#btn-home');
+    // btnAdd.addEventListener('click', (e) => {
+    //   // hide our user interface that shows our A2HS button
+    //   btnAdd.style.display = 'none';
+    //   // Show the prompt
       deferredPrompt.prompt();
-      // Wait for the user to respond to the prompt
+    //   // Wait for the user to respond to the prompt
       deferredPrompt.userChoice
         .then((choiceResult) => {
           if (choiceResult.outcome === 'accepted') {
@@ -66,7 +66,7 @@ export function register(config) {
           }
           deferredPrompt = null;
         });
-    });
+    // });
 	}
 
 	if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
