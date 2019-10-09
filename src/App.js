401368415848040a748f218291361e9e9249add1
Mayur Panchal <mayurpanchal002@gmail.com>
2019-10-09 16:14:41 +0530

    added google smartlock

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Todo from './components/todo';
+// import Todo from './components/todo';
 import Header from './components/header';
-import Preview from './components/preview';
+// import Preview from './components/preview';
 import './App.scss';
 
 const App = () => {
--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { rootReducer } from './components/util/rootReducer';
+import React from "react";
+import ReactDOM from "react-dom";
+import { YoloProvider } from "react-googleyolo";
+import "./index.scss";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { rootReducer } from "./components/util/rootReducer";
 
 const store = createStore(rootReducer);
 
 ReactDOM.render(
-	<Provider store={store}>
-		<App />
-	</Provider>,
-	document.getElementById('root')
+  <YoloProvider
+    clientId={process.env.REACT_APP_GID}
+    onRetrieveSuccess={console.log}
+    onRetrieveFailure={console.log}
+  >
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </YoloProvider>,
+  document.getElementById("root")
 );
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
